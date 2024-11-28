2e29f9c07556e6c207b4e5d1b3fe3293ab0c11ea
Catharina <catharina2002@gmail.com>
2024-11-28 14:49:31 +0100

    radix-test

---
+++ src/app/components/MyApp.jsx
@@ -0,0 +1,14 @@
+import { Flex, Text, Button } from "@radix-ui/themes";
+import { BookmarkIcon } from "@radix-ui/react-icons"
+
+export default function MyApp() {
+	return (
+		<Flex direction="column" gap="2">
+			<Text>Radix Themes</Text>
+            <Button variant="soft">
+		<BookmarkIcon /> Bookmark
+	</Button>
+
+		</Flex>
+	);
+}
---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+import MyApp from "./components/MyApp";
+
+export default function () {
+	return (
+		<Theme>
+			<MyApp/>
+		</Theme>
+	);
+}
+
+
